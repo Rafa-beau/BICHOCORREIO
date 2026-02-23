04695f5e1e591f449167d91d63b723e71e8b5cda
Thur :) <thurgithub@gmail.com>
2026-02-23 04:18:55 -0300

    table.gd cleaned

--- bichocorreio/script/table.gd
+++ bichocorreio/script/table.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
-@onready var card = ("res://node/card")
-@onready var player = get_node("Life/Player")
+@export var card_scene: PackedScene
+@export var player: GDScript
 
-var card_scene := preload("res://node/card.tscn")
 var parent = self
 var vel = 15
 var current_card
@@ -15,6 +14,8 @@ func _ready() -> void:
 	SignalManager.accept.connect(accept)
 	SignalManager.reject.connect(reject)
 
+
+### SPAWNAR CARTA
 func call_card():
 	var des_summon = Vector2(98, 85)
 	
@@ -26,6 +27,7 @@ func call_card():
 	tween.tween_property(current_card, "position", des_summon, 0.55).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 	SignalManager.call_card.emit()
 
+### AÇÔES DA CARTA
 func reject():
 	player.take_damage(1)
 	current_card.queue_free()
@@ -37,63 +39,69 @@ func accept():
 	current_card.queue_free()
 	call_card()
 	print("ceitou")
+	
+func next_card():
+	if current_card:
+		current_card.queue_free()
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.name.begins_with("Card"):
-		current_card = area
+### MOVIMENTO
+func move_card(des):
+	var tween = create_tween()
+	tween.tween_property(current_card, "position", des, 0.55).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+	await get_tree().create_timer(0.60).timeout
+	return
+
+### VALIDATORES
+func accept_validate() -> bool:
+	if current_card.water == true and current_card.water_stamp == true:
+		current_card.water = false
+		return true
+	if current_card.water == false and current_card.water_stamp == false and current_card.stamped == true:
+		return false
+	return false
+	
+func confiscate_validate() -> bool:
+	# troocar para as validacoes certas dps
+	if current_card.water == true and current_card.water_stamp == true:
+		current_card.water = false
+		return true
+	if current_card.water == false and current_card.water_stamp == false and current_card.stamped == true:
+		return false
+	return false
 		
-
+### ACEITAR E CONFISCAR - CLICK
 func _on_accept_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	var des_accept = Vector2(1500, 85)
 	if pull == true and current_card:
 		if event.is_action_released("click"):
 			pull = false
-			var tween = create_tween()
-			tween.tween_property(current_card, "position", des_accept, 0.55).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
-			await get_tree().create_timer(0.60).timeout
-			if current_card.water == true:
-				if current_card.water_stamp == true:
-					current_card.water == false
-					accept()
-				else:
-					reject()
-			elif current_card.water == false:
-				if current_card.water_stamp == false and current_card.stamped == true:
-					accept()
-				else:
-					reject()
+			await move_card(Vector2(1500, 85))
+			if accept_validate() == true:
+				accept()
+				return
+			reject()
 
+func _on_confiscate_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if pull == true and current_card :
+		if event.is_action_released("click"):
+			pull = false
+			await move_card(Vector2(198, 1000))
+			if confiscate_validate() == true:
+				accept()
+				return
+			reject()
+			
+### ACEITAR E CONFISCAR - MOUSE ENTERED
 func _on_accept_mouse_entered() -> void:
 	if current_card:
 		if current_card.dragging == true:
 			pull = true
-		else:
 			return
-
-func _on_confiscate_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	var des_confis = Vector2(198, 1000)
-	if pull == true and current_card :
-		if event.is_action_released("click"):
-			pull = false
-			var tween = create_tween()
-			tween.tween_property(current_card, "position", des_confis, 0.55).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
-			await get_tree().create_timer(0.60).timeout
-			if current_card.water == false and current_card.water_stamp == false:
-				print("accept")
-				SignalManager.accept.emit()
-			else:
-				print("reject")
-				reject()
-			if current_card.water == true and current_card.water_stamp == true:
-				print("accept")
-				SignalManager.accept.emit()
-			else:
-				print("reject")
-				SignalManager.reject.emit()
+		return
+			
 
 func _on_confiscate_mouse_entered() -> void:
 	if current_card:
 		if current_card.dragging == true:
 			pull = true
-		else:
 			return
+		return
