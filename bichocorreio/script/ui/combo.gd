93e18a3c717dfeeb22d28b9fb95bf7ddc720b025
Thur :) <thurgithub@gmail.com>
2026-02-28 01:40:28 -0300

    AAAAAAAAAAAAAAAAAA ACABA FELPO JAM ACABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- bichocorreio/script/ui/combo.gd
+++ bichocorreio/script/ui/combo.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var text = $cur_combo
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var combo_count: int = 0
 var combo_timer: float = 0.0
@@ -9,7 +10,7 @@ const combo_time: float = 5.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	z_index = 1000
-	self.hide()
+	hide()
 	pass # Replace with function body.
 
 func _process(delta: float):
@@ -18,15 +19,24 @@ func _process(delta: float):
 		if combo_timer <= 0:
 			reset_combo()
 
+func show_combo():
+	show()
+	if combo_count > 1:
+		return
+	animation_player.play("oi")
+	
+
 func add_combo():
 	combo_count += 1
 	combo_timer = combo_time
-	self.show()
+	show_combo()
 	ui_update()
 
 func reset_combo():
+	animation_player.play("tchau")
+	await Utils.timer(0.5)
 	combo_count = 0
-	self.hide()
+	hide()
 
 func ui_update():
-	text.text = "x" + str(combo_count)
+	text.text = "x[shake]" + str(combo_count)
